111f1c3c29567f2cdb00f77f00d88c1aaab9d54e
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-03-25 15:19:27 +0100

    début design tableau de bord

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -8,17 +8,65 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
+    Size mqSize = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-          title: Text("Tableau de bord"),
+        title: Text("Tableau de bord"),
         actions: [
-          TextButton(
+          IconButton(
+              onPressed: null,
+              icon: Icon(Icons.refresh, color: Colors.white), iconSize: 30
+          ),
+          IconButton(
               onPressed: (() => Navigator.pushNamed(context, '/informations')),
-              child: Text("Informations")
+              icon: Icon(Icons.account_circle, color: Colors.white,), iconSize: 30
           )
         ],
       ),
-      body: Center(),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Card(
+                elevation: 10.0,
+                child: InkWell(
+                  onTap: (() {
+                    print('ok');
+                  }),
+                  child: Container(
+                    height: mqSize.height / 8,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('Ajouter un verre', style: TextStyle(
+                            fontSize: 40
+                        )),
+                        Image.asset('images/beer.png',
+                            width: mqSize.width / 6)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 10.0,
+                child: Container(
+                  height: mqSize.height / 3,
+                  width: mqSize.width,
+                  child: Center(
+                    child: Text("0g/L", style: TextStyle(
+                        fontSize: 80
+                    )),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
