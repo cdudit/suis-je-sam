e2aad534cbb787492cfbdab8c1286449001cc1c9
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-03-25 09:16:30 +0100

    ajout du fab et disposition des cards

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        brightness: Brightness.light
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark
-      ),
+      theme:
+          ThemeData(primarySwatch: Colors.blue, brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.dark,
-      home: MyHomePage(title: 'Dashboard'),
+      home: MyHomePage(title: 'Vos informations'),
     );
   }
 }
@@ -33,6 +29,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double cardElevation = 10.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,7 +38,41 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Card(
+                elevation: cardElevation,
+                child: Container(
+                  height: 125,
+                ),
+              ),
+              Card(
+                elevation: cardElevation,
+                child: Container(
+                  height: 125,
+                ),
+              ),
+              Card(
+                elevation: cardElevation,
+                child: Container(
+                  height: 125,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        onPressed: null,
+        child: Icon(
+          Icons.check,
+          color: Colors.white,
+          size: 40.0,
+        ),
       ),
     );
   }
