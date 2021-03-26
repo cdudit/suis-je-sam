1e46119750aef29df98f93dd4dd50f30ae73e177
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-03-26 11:50:24 +0100

    ajout du calcul en fonction de la durée

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
@@ -14,12 +15,23 @@ class _DashboardState extends State<Dashboard> {
   int userWeight;
   double userGenderTx;
   Size mqSize;
+  int startDate;
+  int currentDate;
+  Timer timer;
 
   @override
   void initState() {
     super.initState();
     // Récupération des informations dans les shared préférences
     getShared();
+    timer = Timer.periodic(
+        Duration(seconds: 60), (Timer t) => calculTauxEveryMinute());
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   @override
@@ -86,8 +98,7 @@ class _DashboardState extends State<Dashboard> {
                         onPressed: null,
                         child: Text(drinked.toString(),
                             style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.grey[800])),
+                                fontSize: 30, color: Colors.grey[800])),
                       ),
                     )
                   ])),
@@ -116,14 +127,13 @@ class _DashboardState extends State<Dashboard> {
   }
 
   RoundedRectangleBorder roundedShape() {
-    return RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20.0));
+    return RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0));
   }
 
   void helpDialog() {
     AlertDialog alert = AlertDialog(
-      shape: roundedShape(),
-      title: Text("Aide", style: TextStyle(fontSize: 35.0)),
+        shape: roundedShape(),
+        title: Text("Aide", style: TextStyle(fontSize: 35.0)),
         content: Container(
           height: mqSize.height / 5,
           child: Column(
@@ -135,7 +145,8 @@ class _DashboardState extends State<Dashboard> {
                   Image.asset('images/beer.png', width: mqSize.width / 6),
                   Container(
                     width: mqSize.width / 2,
-                    child: helpText('Une bière de 8°, vous choisissez la quantité ensuite.'),
+                    child: helpText(
+                        'Une bière de 8°, vous choisissez la quantité ensuite.'),
                   )
                 ],
               ),
@@ -145,14 +156,13 @@ class _DashboardState extends State<Dashboard> {
                   Image.asset('images/wine.png', width: mqSize.width / 6),
                   Container(
                     width: mqSize.width / 2,
-                    child: helpText('Un verre de vin de 14cL à 12°'),
+                    child: helpText('Un verre de vin de 14cL à 12°.'),
                   )
                 ],
               )
             ],
           ),
-        )
-    );
+        ));
     showDialog(
       context: context,
       builder: ((BuildContext context) => alert),
@@ -167,42 +177,67 @@ class _DashboardState extends State<Dashboard> {
   /// Augmentation du taux d'alcoolémie
   void incrementTaux(int mL, double degree) {
     setState(() {
+      if (drinked == 0) {
+        // Récupération du timestamp du premier verre bu
+        startDate =
+            (DateTime.now().millisecondsSinceEpoch / 1000 / 60 / 15).round();
+      }
+      currentDate =
+          (DateTime.now().millisecondsSinceEpoch / 1000 / 60 / 15).round();
+
+      // Incrémentation du nombre de verres bu
       drinked++;
-      // (mL * degrés * densité de l'alcool) / (poids * taux)
-      taux += (mL * degree * 0.8) / (userWeight * userGenderTx);
+
+      // Calcul du taux
+      // ((mL * degrés * densité de l'alcool) / (poids * taux)) - (txElimination * (nbQuartHeureMtn-nbQuartHeurePremierVerre))
+      taux += ((mL * degree * 0.8) / (userWeight * userGenderTx)) -
+          (((userGenderTx == 0.7) ? 0.025 : 0.02125) *
+              (currentDate - startDate));
     });
+  }
+
+  /// Calcul du taux toutes les minutes
+  void calculTauxEveryMinute() {
+    if (drinked > 0) {
+      setState(() {
+        currentDate =
+            (DateTime.now().millisecondsSinceEpoch / 1000 / 60 / 15).round();
+        taux -= ((userGenderTx == 0.7) ? 0.025 : 0.02125) *
+            (currentDate - startDate);
+      });
+    }
   }
 
   /// Affichage du dialog pour choisir la quantité
   void displayDialog() {
     // Création du dialog
     AlertDialog alert = AlertDialog(
-      shape: roundedShape(),
+        shape: roundedShape(),
         content: Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(
-              onPressed: (() {
-                incrementTaux(250, 0.08);
-                Navigator.pop(context);
-              }),
-              child: dialogStyle("25cl")),
-          TextButton(
-              onPressed: (() {
-                incrementTaux(330, 0.08);
-                Navigator.pop(context);
-              }),
-              child: dialogStyle("33cL")),
-          TextButton(
-              onPressed: (() {
-                incrementTaux(500, 0.08);
-                Navigator.pop(context);
-              }),
-              child: dialogStyle("50cL"))
-        ],
-      ),
-    ));
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: (() {
+                    incrementTaux(250, 0.08);
+                    Navigator.pop(context);
+                  }),
+                  child: dialogStyle("25cl")),
+              TextButton(
+                  onPressed: (() {
+                    incrementTaux(330, 0.08);
+                    Navigator.pop(context);
+                  }),
+                  child: dialogStyle("33cL")),
+              TextButton(
+                  onPressed: (() {
+                    incrementTaux(500, 0.08);
+                    Navigator.pop(context);
+                  }),
+                  child: dialogStyle("50cL"))
+            ],
+          ),
+        ));
     // Affichage du dialog
     showDialog(
       context: context,
